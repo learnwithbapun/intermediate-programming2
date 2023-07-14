193ed7340e8312b9ade9f593c8305b3d4c26a783
Bhagabata Parida <learnwithbapun@gmail.com>
2023-07-14 17:57:28 +0530

    Added

--- src/main/java/string50/LessonQuestion4.java
+++ src/main/java/string50/LessonQuestion4.java
@@ -2,19 +2,43 @@ package string50;
 
 // Given a String calculate the length of longest palindrome substring.
 
-
 public class LessonQuestion4 {
 
-	public static boolean checkPalindrome(char ch[]) {
-		
+	public static int checkPalindrome(char ch[]) {
 
-	return true;
+		// ODD LENGTH SUBSTRING
+		int n = ch.length;
+		int max = 0;
+		for (int i = 0; i < n; i++) {
+			int len = expand(ch, i, i);
+			max = Math.max(max, len);
+		}
+
+		// EVEN LENGTH SUBSTRING
+
+		for (int i = 0; i < n - 1; i++) {
+			int len = expand(ch, i, i + 1);
+			max = Math.max(max, len);
+		}
+
+		return 0;
+	}
+
+	private static int expand(char[] ch, int i, int i2) {
+		int n = ch.length;
+		while (i >= 0 && i2 < n && ch[i] == ch[i2]) {
+			i--;
+			i2++;
+
+		}
+
+		return (i2 - i - 1);
 	}
 
 	public static void main(String[] args) {
-//		char ch[] = { 'b', 'a', 'd', 'a', 'm', 'b', 'e', 'c' };
-		char ch[] = { 'r', 'a', 'd', 'a', 'r' };
-		System.out.println((checkPalindrome(ch)));
+		char ch[] = { 'x', 'b', 'd', 'y', 'z', 'z', 'y', 'd', 'b', 'd', 'b', 'd', 'y', 'z', 'y', 'd', 'x' };
+		System.out.println(ch.length);
+		System.out.println(checkPalindrome(ch));
 
 	}
 
